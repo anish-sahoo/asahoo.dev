f0c5b71ff613518928903926f8f0cc7f068c3c0a
Anish Sahoo <anishsahoo2005@gmail.com>
2024-01-28 14:31:49 -0800

    Added links

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -18,30 +18,35 @@ const Contact = () => {
           </a>
         </p>
         <p>You can also find me at:</p>
-        <Button
-          className="my-2 bg-gray-950 text-white text-sans font-bold text-md h-14 pr-8 w-52"
-          startContent={
-            <img
-              src={githubLogo}
-              className="object-scale-down h-10 w-auto p-2 px-1 pr-4 rounded-lg"
-              alt="GitHub Logo"
-            ></img>
-          }
-        >
-          My GitHub
-        </Button>
-        <Button
-          className="my-2 bg-blue-600 text-sans font-bold text-white text-md h-14 w-52"
-          startContent={
-            <img
-              src={linkedInLogo}
-              className="object-scale-down h-10 w-auto p-2 px-1 rounded-lg bg-white"
-              alt="LinkedIn Logo"
-            ></img>
-          }
-        >
-          My LinkedIn
-        </Button>
+
+        <a href="https://github.com/anish-sahoo">
+          <Button
+            className="my-2 bg-gray-950 text-white text-sans font-bold text-md h-14 pr-8 w-52"
+            startContent={
+              <img
+                src={githubLogo}
+                className="object-scale-down h-10 w-auto p-2 px-1 pr-4 rounded-lg"
+                alt="GitHub Logo"
+              ></img>
+            }
+          >
+            My GitHub
+          </Button>
+        </a>
+        <a href="https://linkedin.com/in/anish-sahoo/">
+          <Button
+            className="my-2 bg-blue-600 text-sans font-bold text-white text-md h-14 w-52"
+            startContent={
+              <img
+                src={linkedInLogo}
+                className="object-scale-down h-10 w-auto p-2 px-1 rounded-lg bg-white"
+                alt="LinkedIn Logo"
+              ></img>
+            }
+          >
+            My LinkedIn
+          </Button>
+        </a>
       </div>
     </Layout>
   );
