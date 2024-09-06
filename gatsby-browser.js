9e4fa965df7d6d4be56ebf98cdc214ea47b273b9
Anish Sahoo <anishsahoo2005@gmail.com>
2024-09-05 20:43:54 -0400

    Add stuff

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,1 @@
 import "./src/styles/global.css";
-
-// export const onInitialClientRender = () => {
-//   const plausibleScript = document.createElement('script');
-//   plausibleScript.setAttribute('async', true);
-//   plausibleScript.setAttribute('defer', true);
-//   plausibleScript.setAttribute('data-domain', 'asahoo.dev');
-//   plausibleScript.src = 'https://analytics.asahoo.dev/js/script.js';
-//   document.body.appendChild(plausibleScript);
-// };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
+    <meta
+      key="referrer-policy"
+      name="referrer"
+      content="no-referrer-when-downgrade"
+    />,
+
     <script
       key="analytics"
       defer
